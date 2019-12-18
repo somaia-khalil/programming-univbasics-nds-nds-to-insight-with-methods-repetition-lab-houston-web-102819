0df8c497a4ab01f6eb5aeaa5d3e69b6e94bab5c1
Iam H <sumaya_khair@live.com>
2019-12-18 16:41:23 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -48,9 +48,7 @@ end
   dir_to_earnings_hash = directors_totals(source)
   dir_names = list_of_directors(source)
   i = 0
-
   total = 0
-
   while i < dir_names.length do
     dir_name = dir_names[i]
     total += dir_to_earnings_hash[dir_name]
